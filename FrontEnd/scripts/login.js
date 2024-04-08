b5d9adbdded7caa42043bc6e005e953175873915
Youssef Labouiti <labouiti.youssef@gmail.com>
2024-04-08 17:20:04 +0200

    Finir la partie connexion déconnexion

--- FrontEnd/scripts/login.js
+++ FrontEnd/scripts/login.js
@@ -1,7 +1,32 @@
+// Vérifier l'état de connexion 
+function islogged(){
+  const token = localStorage.getItem("token");
+  const mode = document.querySelector(".edition-mode");
+  const buttonFiltres = document.querySelector(".filtres");
+  const buttonModifier = document.querySelector(".title span");
+  
+  if (mode && buttonFiltres && buttonModifier) {
+     if (token) {
+      // Afficher le mode édition et le button modifier
+       mode.style.display= "flex"
+       buttonModifier.style.display= "block"
+       // Masquer les buttons des filtres
+       buttonFiltres.style.display= "none"
+       
+     } else {
+      // Masquer le mode édition et le button modifier
+       mode.style.display= "none"
+       buttonModifier.style.display= "none"
+        // Afficher les buttons des filtres
+       buttonFiltres.style.display= "flex"
+      }
+  }
+}
 
-//Connexion 
+// Fonction de Connexion 
 function login(){
     const form= document.getElementById("loginForm");
+    if(form){
     form.addEventListener("submit", async (event)=>{
         event.preventDefault();
         const email= document.getElementById("email").value;
@@ -9,7 +34,7 @@ function login(){
         const loginErreur= document.querySelector(".passwordErr");
 
         try {
-            // Appel de la fonction fetch avec toutes les informations nécessaires
+            // Appel de la fonction fetch 
               const response = await fetch("http://localhost:5678/api/users/login", {
               method: "POST",
               headers: { "Content-Type": "application/json" },
@@ -21,22 +46,62 @@ function login(){
               // Affichage du message d'erreur en cas d'identifiants incorrects
               loginErreur.textContent = "Erreur dans l’identifiant ou le mot de passe";
                }
-
             const result = await response.json();
               // Stockage du token dans le local storage
             localStorage.setItem("token", result.token);
             
             // Redirection vers la page d'accueil
-            window.location.href ="./index.html";
-
+            window.location.href ="index.html"
         } 
-        catch (error) {
+       catch (error) {
             // Message en cas d'erreurs de requête ou de connexion
             console.error("Erreur lors de la requête d'authentification:", error);
-        }   
-    });
+        } 
+      finally{
+        islogged();
+      }
+                
+    }); 
+  }
+    
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    login();
+// Fonction de déconnexion
+function logout() {
+  const loginli = document.querySelector(".loginli");
+  const logoutli = document.querySelector(".logoutli");
+
+  if (loginli && logoutli){
+
+    // Vérification si le token est déjà stocké dans le local storage
+    if (localStorage.getItem("token")) {
+      // Masquer "Login" et Afficher "Logout" dans le menu
+      loginli.style.display= "none"
+      logoutli.style.display= "block"
+
+      logoutli.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        // Suppression du token du local storage
+        localStorage.removeItem("token");
+
+        // Redirection vers la page d'identification
+       window.location.href = "./login.html";
+      });
+    } else {
+      // Afficher "Login" après la déconnexion
+      loginli.style.display= "block"
+    }
+  }
+}
+
+ document.addEventListener("DOMContentLoaded", function () {
+  islogged();
+  logout();
+  login();
   });
+
+  /*window.addEventListener("popstate", function (event) {
+    // Vérifier l'état de connexion lorsque l'utilisateur utilise le bouton "retour" du navigateur
+    islogged();
+  });*/
